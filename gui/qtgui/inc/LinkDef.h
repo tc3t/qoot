ab1f669ca13143453a74432de5bbdddb36ad2f52
tc3t <tc3t@users.noreply.github.com>
2014-11-08 12:23:31 +0200

    [qtgui merging] build config fixes.

--- gui/qtgui/inc/LinkDef.h
+++ gui/qtgui/inc/LinkDef.h
@@ -1,16 +1,12 @@
-/* @(#)root/win32:$Name$:$Id: LinkDef.h 2825 2008-08-01 19:24:37Z fine $ */
+/* @(#)root/win32:$Id$ */
 
-/****************************************************************************
-** $Id: LinkDef.h 2825 2008-08-01 19:24:37Z fine $
-**
-** Copyright (C) 2004 by Valeri Fine. Brookhaven National Laboratory.
-**                                    All rights reserved.
-**
-** This file may be distributed under the terms of the Q Public License
-** as defined by Trolltech AS of Norway and appearing in the file
-** LICENSE.QPL included in the packaging of this file.
-**
-*****************************************************************************/
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
 
 #ifdef __CINT__
 
@@ -19,18 +15,13 @@
 #pragma link off all functions;
  
 // #pragma link C++ class TQt;
-#pragma link C++ class TQtGUIFactory;
+#pragma link C++ class TQtRootGuiFactory;
 #pragma link C++ class TBrowserCustom;
-#pragma link C++ class TQtColorSelect;
-#pragma link C++ class TQtPatternSelect;
-#pragma link C++ class TQtMarkerSelect;
 #pragma link C++ class TEmbeddedPad;
-#pragma link C++ class TQtTabValidator;
-#pragma link C++ class TQtZoomPadWidget;
-#pragma link C++ class TQRootCanvas;
-#pragma link C++ class TQtPad2Html;
-#pragma link C++ class TQtCanvas2Html;
+#pragma link C++ class TQtColorSelect;
+#pragma link C++ class TQtMarkerSelect;
+#pragma link C++ class TQtPatternSelect;
 #pragma link C++ class TQtPixmapBox;
-#pragma link C++ class TEmitRootSignal;
-#pragma link C++ class TColorEmit;
+#pragma link C++ class TQtGUIFactory;
+
 #endif
