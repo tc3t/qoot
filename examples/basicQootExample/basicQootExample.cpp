bdbc6daacec58e4c34fe40b626b87a2d3ef00fbf
tc3t <tc3t@users.noreply.github.com>
2014-12-07 18:44:52 +0200

    [examples] basicQootExample: Can now optionally use native GUI.

--- examples/basicQootExample/basicQootExample.cpp
+++ examples/basicQootExample/basicQootExample.cpp
@@ -7,9 +7,14 @@
 #include <TGraph.h>
 #include <TEnv.h>
 #include <TGQt.h>
+#include <TApplication.h>
+#include <memory>
 
 int main(int argc, char* argv[])
 {
+    const double x[] = { 1, 2, 3 };
+    const double y[] = { 2, 3, 2 };
+
     QApplication a(argc, argv);
 
     const auto sGuiFactory = QInputDialog::getItem(nullptr,
@@ -19,27 +24,41 @@ int main(int argc, char* argv[])
 
     gEnv->SetValue("Gui.Factory", sGuiFactory.toLatin1().constData());
 
-    QWidget w;
-    auto pMainLayout = new QGridLayout(&w);
+    const bool bUseQt = (sGuiFactory != "native");
 
-    TQtWidget widget;
-    widget.setWindowTitle("TQtWidget");
-    widget.cd();
+    std::unique_ptr<QWidget> spWidget;
 
-    auto pEdit = new QTextEdit("This is an example to use TQtWidget in a grid layout", &w);
-    pEdit->setMaximumHeight(50);
-    pMainLayout->addWidget(pEdit, 0, 0, 1, 2);
-    pMainLayout->addWidget(&widget, 1, 0);
-    pMainLayout->addWidget(new QSlider(Qt::Vertical, &w), 1, 1);
-    pMainLayout->addWidget(new QSlider(Qt::Horizontal, &w), 2, 0);
+    if (bUseQt)
+    {
+        spWidget.reset(new QWidget);
+        auto& w = *spWidget;
+        auto pMainLayout = new QGridLayout(&w);
 
-    const double x[] = { 1, 2, 3 };
-    const double y[] = { 2, 3, 2 };
-    TGraph graph(3, x, y);
-    graph.Draw("AL*");
-    graph.SetTitle("A graph in TQtWidget");
-    w.resize(800, 600);
-    w.show();
+        auto& widget = *(new TQtWidget(&w));
+        widget.setWindowTitle("TQtWidget");
+        widget.cd();
+
+        auto pEdit = new QTextEdit("This is an example to use TQtWidget in a grid layout", &w);
+        pEdit->setMaximumHeight(50);
+        pMainLayout->addWidget(pEdit, 0, 0, 1, 2);
+        pMainLayout->addWidget(&widget, 1, 0);
+        pMainLayout->addWidget(new QSlider(Qt::Vertical, &w), 1, 1);
+        pMainLayout->addWidget(new QSlider(Qt::Horizontal, &w), 2, 0);
+
+        auto pGraph = new TGraph(3, x, y);
+        pGraph->Draw("AL*");
+        pGraph->SetTitle("A graph in TQtWidget");
+        pGraph->SetBit(kCanDelete);
+
+        w.resize(800, 600);
+        w.show();
+    }
+    else
+        gEnv->SetValue("Gui.Backend", "native");
+
+    std::unique_ptr<TApplication> spRootApp;
+    if (!bUseQt)
+        spRootApp.reset(new TApplication("app", &argc, argv));
 
     //TQtWidget::InitRint(); // Note: If no TQtWidget is created before creating TCanvas, one needs to init ROOT application manually.
     TCanvas canvas("canvas", "TCanvas", 800, 600);
@@ -48,5 +67,8 @@ int main(int argc, char* argv[])
     graph2.Draw("AL*");
     graph2.SetTitle("A graph in canvas widget");
 
-    return a.exec();
+    if (bUseQt)
+        return a.exec();
+    else
+        return spRootApp->Run();
 }
