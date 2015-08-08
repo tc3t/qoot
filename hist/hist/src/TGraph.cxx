tc3t/qoot

commit a103023cc2438dd1b4739b26e7c5540f28163fe3

[mod] TGraph: Apply(), RemovePoint() and SetPoint() will no longer call gPad->Modified(). While this change is expected be a regression to some usage scenarios, global side-effects in such basic functionality is not desired and it's not considered correct to modify gPad if the graph is not associated with it. InsertPoint(), however, will still do the call since it's GUI-related functionality in case which modifying gPad is reasonable.

hist/hist/src/TGraph.cxx
@@ -569,7 +569,7 @@ void TGraph::Apply(TF1 *f)
    for (Int_t i = 0; i < fNpoints; i++) {
       fY[i] = f->Eval(fX[i], fY[i]);
    }
-   if (gPad) gPad->Modified();
+   //if (gPad) gPad->Modified();
 }
 
 
@@ -2106,7 +2106,7 @@ Int_t TGraph::RemovePoint(Int_t ipoint)
 
    Double_t **ps = ShrinkAndCopy(fNpoints - 1, ipoint);
    CopyAndRelease(ps, ipoint + 1, fNpoints--, ipoint);
-   if (gPad) gPad->Modified();
+   //if (gPad) gPad->Modified();
    return ipoint;
 }
 
@@ -2260,7 +2260,7 @@ void TGraph::SetPoint(Int_t i, Double_t x, Double_t y)
    }
    fX[i] = x;
    fY[i] = y;
-   if (gPad) gPad->Modified();
+   //if (gPad) gPad->Modified();
 }
 
 

commit 2efc2f692625bbd30d7760dda9cf5b45c12ad2e5

[regFix] TGraph::RemovePoint(): gPad->Modified() was omitted also in the GUI-version of RemovePoint() (broken in commit a103023c).

hist/hist/src/TGraph.cxx
@@ -2126,7 +2126,9 @@ Int_t TGraph::RemovePoint()
          break;
       }
    }
-   return RemovePoint(ipoint);
+   const auto removedPoint = RemovePoint(ipoint);
+   if (removedPoint >= 0 && gPad)
+       gPad->Modified();
 }
 
 